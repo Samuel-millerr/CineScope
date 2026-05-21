6636545ccba9cbbbb1cb095f362e1171a09ae31c
Samuel-millerr <samuel.m.soares10@aluno.senai.br>
2026-05-21 14:48:00 -0300

    feat: criação do primeiro endpoint de HealthCheck na entidade de Director

--- backend/spring-boot/src/main/java/com/project/cinescope/director/DirectorController.java
+++ backend/spring-boot/src/main/java/com/project/cinescope/director/DirectorController.java
@@ -1,23 +1,49 @@
 package com.project.cinescope.director;
 
+import com.project.cinescope.actor.Actor;
 import com.project.cinescope.director.request.DirectorRequestDto;
 import com.project.cinescope.director.response.DirectorResponseDto;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/directors")
 public class DirectorController {
     private final DirectorService directorService;
+    private final RequestMappingHandlerMapping handlerMapping;
 
-    public DirectorController(DirectorService directorService) {
+    public DirectorController(DirectorService directorService, RequestMappingHandlerMapping handlerMapping) {
         this.directorService = directorService;
+        this.handlerMapping = handlerMapping;
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        Class<?> myClass = this.getClass();
+        List<Map<String, Object>> routes = new ArrayList<>();
+        Map<RequestMappingInfo, HandlerMethod> handlerMethods = handlerMapping.getHandlerMethods();
+
+        for (Map.Entry<RequestMappingInfo, HandlerMethod> entry : handlerMethods.entrySet()) {
+            HandlerMethod handlerMethod = entry.getValue();
+
+            if (handlerMethod.getBeanType().equals(myClass)) {
+                RequestMappingInfo mappingInfo = entry.getKey();
+
+                Set<String> urls = mappingInfo.getDirectPaths();
+                System.out.println(urls);
+//                Map<String, Object> routeInfo = new LinkedHashMap<>();
+            }
+        }
+        return ResponseEntity.ok("OK");
     }
 
     @GetMapping
