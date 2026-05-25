4d2cb2d97ec2ac7316c29e96d6e402d6078831cb
Samuel-millerr <samuel.m.soares10@aluno.senai.br>
2026-05-25 14:22:15 -0300

    style: remoção de imports desnecessários

--- backend/spring-boot/src/main/java/com/project/cinescope/auth/AuthController.java
+++ backend/spring-boot/src/main/java/com/project/cinescope/auth/AuthController.java
@@ -18,7 +18,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
